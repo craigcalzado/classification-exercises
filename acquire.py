64104e124b05b7c88689958871459ad5088c54e3
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-02-24 11:38:19 -0600

    initial commit

---
+++ acquire.py
@@ -0,0 +1,8 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import pydataset as pds
+
+from env import host, user, password

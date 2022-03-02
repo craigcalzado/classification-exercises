515ea1a4115ec40f5e600b0e0413bb7ccf4d7bb3
craigcalzado <96085268+craigcalzado@users.noreply.github.com>
2022-03-02 13:38:47 -0600

    Updated added split_data function

--- prepare.py
+++ prepare.py
@@ -1,4 +1,6 @@
 
+from email.errors import InvalidBase64LengthDefect
+from pdb import post_mortem
 from wsgiref import validate
 import pandas as pd
 import warnings
@@ -14,11 +16,20 @@ df.head()
 def prep_iris(df): # create a function named prep_iris that accepts the untransformed iris data, and returns the data with the transformations above applied.
     df = df.drop(columns=['species_id', 'measurement_id', 'Unnamed: 0']) # drop the species_id and measurement_id columns.
     df = df.rename(columns={'species_name': 'species'}) # rename the species_name column to just species.
-    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=True) # create dummy variables of the species name and concatenate onto the iris dataframe. (This is for practice, we don't always have to encode the target, but if we used species as a feature, we would need to encode it).
+    dummy_df = pd.get_dummies(df[['species']], drop_first=True) # create dummy variables of the species name and concatenate onto the iris dataframe. (This is for practice, we don't always have to encode the target, but if we used species as a feature, we would need to encode it).
     df = pd.concat([df, dummy_df], axis=1) # concatenate the iris dataframe with the dummy variables.
-    return df # drop the species column.
+    return df
 
 prep_iris(df) # call the function prep_iris to apply the transformations to the iris data.
+df.head()
+split_data(df)
+
+def split_iris_data(df):
+    train, test = train_test_split(df, test_size=0.2, random_state=789)
+    train, validate = train_test_split(train, test_size=0.3, random_state=789)
+    return train, validate, test
+split_iris_data(df)
+
 
 #------------------------------------------------------------------------------------------------------------
 
@@ -30,9 +41,16 @@ def prep_titanic(df):
     dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=[True,True])
     df = pd.concat([df, dummy_df], axis=1)
     return df.drop(columns=['sex', 'embark_town'])
-prep_titanic(df)
+df = prep_titanic(df)
 
-# titanic split data
+#split titanic data
+def split_titanic_data(df):
+    train, test = train_test_split(df, test_size=.2, random_state=789, stratify=df.survived)
+    train, validate = train_test_split(train, test_size=.3, random_state=789, stratify=train.survived)
+    return train, validate, test
+split_titanic_data(df)
+
+split_dataframe(df)
 
 def prep_telco(df):
     df = acquire.get_telco_data()
@@ -40,4 +58,28 @@ def prep_telco(df):
     dummy_df = pd.get_dummies(df[['gender', 'payment_type', 'contract_type', 'internet_service_type']], drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
     return df.drop(columns=['gender', 'payment_type', 'contract_type', 'internet_service_type'])
-prep_telco(df)
+df = prep_telco(df)
+
+
+# function to split dataframes
+def split_dataframe(df):
+   train, test = train_test_split(df, test_size=0.2, random_state=789)
+   train, validate = train_test_split(train, test_size=0.3, random_state=789)
+   return train, validate, test 
+
+
+
+# how many lottery tickets would you have to buy in order to win.
+purchase each day
+citys with the tickets
+how much to Invest
+    
+    # how many lottery tickets would you have to buy in order to win.
+    purchase each day
+    citys with the tickets
+    how much to Invest
+
+beteen how many people to split the pot
+everyone in the US dived by 2
+in the regioun
+
